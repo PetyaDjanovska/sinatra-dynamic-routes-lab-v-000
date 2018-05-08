e7654c5f774842dbdcfce006ec853173261bd541
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-08 15:31:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,10 +12,6 @@ class App < Sinatra::Base
 
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
-    i = 0
-    until i <= @num do
-      "#{params[:phrase]}\n"
-            i += 1
-    end
+      "#{params[:phrase]}"
   end
 end
